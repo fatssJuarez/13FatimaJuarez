a2ef014ad3843d49954700b4e93ad0a3065a378c
fatssJuarez <162488235+fatssJuarez@users.noreply.github.com>
2024-03-11 21:23:13 -0600

    Update PARoINPAR.java

--- PARoINPAR/src/main/java/com/mycompany/paroinpar/PARoINPAR.java
+++ PARoINPAR/src/main/java/com/mycompany/paroinpar/PARoINPAR.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.paroinpar;
 
+report java.util.Scanner;
 /**
  *
  * @author maria
@@ -11,7 +12,15 @@ package com.mycompany.paroinpar;
 public class PARoINPAR {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Scanner sc = new Scanner (System.in);
+        System.out.println ("Programa que indica si un numero es par o inpar");
+        System.out.println("Coloque un numero: ");
+        int num = sc.nextInt();
+        if (num % 2 == 00 ){
+            System.out.println("El numero ingresado es par");
+        }else{
+            System.out.println("El numero ingresado es inpar");
+        }
     }
-    pruebaaa.........................................
+    
 }
